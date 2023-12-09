83778da0e079a89a35cb41029868d92916a13faa
zeroone-kr <dnjsdlf61@naver.com>
2023-12-09 13:49:57 +0900

    chore: restore removed files

---
+++ class_basic.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+class Animal {
+
+private:
+	int food;
+	int weight;
+
+public:
+	void set_animal(int _food, int _weight);
+	void increase_food(int inc);
+	void view_stat();
+
+};
+
+
+void Animal::set_animal(int food, int weight) {
+
+	this->food = food;
+	this->weight = weight;
+
+}
+
+void Animal::increase_food(int inc) {
+
+	this->food += inc;
+	this->weight += (inc / 3);
+
+}
+
+void Animal::view_stat() {
+	
+	cout << "이 동물의 food: " << this->food << endl;
+	cout << "이 동물의 weight: " << this->weight << endl;
+	
+}
+
+
+int main() {
+
+	Animal animal;
+	animal.set_animal(100, 50);
+	animal.increase_food(30);
+	animal.view_stat();
+	return 0;
+
+}
---
+++ explicit.cpp
@@ -0,0 +1,65 @@
+// 컴파일 에러 나야함
+#include <iostream>
+
+class MyString{
+
+    char *string_content;
+    int string_length;
+    int memory_capacity;
+
+public:
+    // explicit keyword: 명시적으로 생성자를 부를 때 에만 허용
+    explicit MyString(int capacity);
+
+    MyString(const char *str);
+
+    // 복사 생성자
+    MyString(const MyString& str);
+
+    ~MyString();
+
+    int length() const;
+
+};
+
+MyString::MyString(int capacity){
+    
+    string_content = new char[capacity];
+    string_length = 0;
+    memory_capacity = capacity;
+
+}
+
+MyString::MyString(const char *str){
+
+    string_length =0;
+    while(str[string_length++]){
+    }
+    string_content = new char[string_length];
+    memory_capacity = string_length;
+    for(int i=0; i != string_length; i++) string_content[i] = str[i];
+
+}
+
+MyString::MyString(const MyString& str){
+    string_length = str.string_length;
+    string_content = new char[string_length];
+
+    for(int i=0 ;i != string_length; i++){
+        string_content[i] = str.string_content[i];
+    }
+}
+
+
+MyString::~MyString() { delete[] string_content; }
+
+int MyString::length() const { return string_length; }
+
+void DoSomethingWithString(MyString s) {
+    // Do something...
+}
+
+int main() {
+    DoSomethingWithString(3); // ????
+}
+
