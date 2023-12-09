0480520a446b880a11d083514e7d334b6903605b
zeroone-kr <dnjsdlf61@naver.com>
2023-12-09 13:16:17 +0900

    featre: add algorithm dir

---
+++ algorithm/sort.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename Iter>
+void print(Iter begin, Iter end){
+
+    while(begin != end){
+        std::cout << *begin << " ";
+        begin++
+    }
+    std::cout << sted::endl;
+
+}
+
+int main(){
+    
+    std::vector<int> vec;
+    vec.push_back(5);
+    vec.push_back(3);
+    vec.push_back(1);
+    vec.push_back(6);
+    vec.push_back(4);
+    vec.push_back(7);
+    vec.push_back(2);
+    
+    std::cout << "정렬 전 ----" <<std::endl;
+    print(vec.begin(), vec.end());
+    std::sort(vec.begin(), vec.end());
+
+    std::cout << "정렬 후 ----" << std::endl;
+    print(vec.begin(), vec.end())
+
+}
